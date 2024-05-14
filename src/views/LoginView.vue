ba197d8088ef9b8ca2dcd1e0be335d56bf7711a9
Nicolas Faessel <niko@localhost.localdomain>
2024-05-14 09:57:08 +0200

    [ADD] login view, dev server proxy config

---
+++ src/views/LoginView.vue
@@ -0,0 +1,34 @@
+<template>
+  <label for="email">Email</label>
+  <input type="email" name="email" id="remail" v-model="email" />
+  <br />
+  <label for="password">Password</label>
+  <input type="password" name="password" id="password" v-model="password" />
+  <button type="button" @click="login">Se connecter</button>
+</template>
+<script setup lang="ts">
+import axiosInstance from '@/modules/axios'
+import { type Ref, ref } from 'vue'
+
+const email: Ref<string> = ref('test@example.com')
+const password: Ref<string> = ref('password')
+
+function login() {
+  axiosInstance
+    .get('/sanctum/csrf-cookie')
+    .then((response) => {
+      console.log('csrf token ok, trying to login')
+      axiosInstance
+        .post('/login', { email: email.value, password: password.value })
+        .then((result) => {
+          console.log('Connexion réussie')
+        })
+        .catch((err) => {
+          console.log('invalid credential')
+        })
+    })
+    .catch((error) => {
+      console.log('error while getting csrf token')
+    })
+}
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,12 @@ export default defineConfig({
   },
   server: {
     proxy: {
+      '/api/sanctum/csrf-cookie': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
       '/api': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
