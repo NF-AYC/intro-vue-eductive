dccb544cda8c68b938cbb3d28f9a4af457fb6213
Nicolas Faessel <niko@localhost.localdomain>
2024-06-13 12:15:46 +0200

    [ADD] manifest property in VitePWA options, and image for manifest correctness

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,47 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
+      },
+      manifest: {
+        name: 'Todo PWA app',
+        short_name: 'Todo',
+        description:
+          "Une application installable et utilisable hors ligne pour gérer les choses que l'on doit faire",
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'todo-icon-192x192.png', // <== don't add slash, for testing
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/todo-icon-512x512.png', // <== don't remove slash, for testing
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'todo-icon-512x512.png', // <== don't add slash, for testing
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: ['any', 'maskable'] // testing new type declaration
+          }
+        ],
+        screenshots: [
+          {
+            src: 'todo-app-screenshot-wide.png',
+            sizes: '863x469',
+            type: 'image/png',
+            form_factor: 'wide',
+            label: 'Todo app screenshot'
+          },
+          {
+            src: 'todo-app-screenshot-wide.png',
+            sizes: '863x469',
+            type: 'image/png',
+            form_factor: undefined,
+            label: 'Todo app screenshot'
+          }
+        ]
       }
     })
   ],
