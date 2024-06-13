0b509e1c06c9e1d3d8334e03034f0c2623048118
Nicolas Faessel <niko@localhost.localdomain>
2024-06-13 11:56:37 +0200

    [ADD] Background sync for POST/PUT/DELETE retry

--- src/sw.ts
+++ src/sw.ts
@@ -5,7 +5,8 @@ import {
 } from 'workbox-precaching'
 import { NavigationRoute, registerRoute, setDefaultHandler } from 'workbox-routing'
 import { clientsClaim } from 'workbox-core'
-import { NetworkFirst, Strategy } from 'workbox-strategies'
+import { NetworkFirst, NetworkOnly, Strategy } from 'workbox-strategies'
+import { BackgroundSyncPlugin } from 'workbox-background-sync'
 
 declare let self: ServiceWorkerGlobalScope
 
@@ -45,6 +46,34 @@ const allowlist: undefined | RegExp[] = [/^\/$/]
 registerRoute(new NavigationRoute(createHandlerBoundToURL('index.html'), { allowlist }))
 
 setDefaultHandler(new NetworkFirst())
+
+const bgSyncPlugin = new BackgroundSyncPlugin('api-retry-queue', {
+  maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
+})
+
+registerRoute(
+  /\/api\/.*/,
+  new NetworkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  'POST'
+)
+
+registerRoute(
+  /\/api\/.*/,
+  new NetworkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  'PUT'
+)
+
+registerRoute(
+  /\/api\/.*/,
+  new NetworkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  'DELETE'
+)
 
 // this is necessary, since the new service worker will keep on skipWaiting state
 // and then, caches will not be cleared since it is not activated
