136c94f54952c5bee5457c4388b06cc2dbdf535b
Nicolas Faessel <niko@localhost.localdomain>
2024-06-13 10:19:43 +0200

    [FIX] import workbox pour fonctionnement hors-ligne fonctionnel

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,41 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      strategies: 'generateSW',
+      // srcDir: 'src',
+      // filename: 'sw.ts',
+      devOptions: {
+        enabled: true,
+        type: 'module',
+        navigateFallback: '/'
+      },
+      workbox: {
+        // globPatterns: [/^\/$/],
+        //        ['**/*.{js,ts,vue,css,html,ico,png,svg,webp}'],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith('/api')
+            },
+            handler: 'NetworkFirst' as const,
+            options: {
+              cacheName: 'api-cache',
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
